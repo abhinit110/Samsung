e4615e22b2ebbdc4a6b3a111f669098e1cc3e504
Abhinit Singh <abhinit710@gmail.com>
2024-08-06 15:36:43 +0530

    Update Stock buy and sell II.cpp

--- Stock buy and sell II.cpp
+++ Stock buy and sell II.cpp
@@ -35,3 +35,44 @@
         return ans;
     }
 };
+//////////////////////////////////////////////////////////
+
+public:
+    // int solve(vector<int>&price, int n, int in, int var)
+    // {
+    //     if(in>=n)
+    //     return 0;
+        
+    //     if(var == 0)
+    //     {
+    //         return max(-price[in]+solve(price,n,in+1,1),solve(price,n,in+1,0));
+    //     }
+    //     else
+    //     {
+    //         return max(price[in]+solve(price,n,in+1,0),solve(price,n,in+1,1));
+    //     }
+    // }
+    int stockBuyAndSell(int n, vector<int> &price) {
+        // code here
+        //return solve(prices,n,0,0);
+        vector<vector<int>>dp(n+1,vector<int>(2,0));
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=0;j<=1;j++)
+            {
+                if(j==0)
+                dp[i][j]=max(-price[i]+dp[i+1][1],dp[i+1][0]);
+                else
+                dp[i][j]=max(price[i]+dp[i+1][0],dp[i+1][1]);
+            }
+        }
+        //  for(int i=0;i<=n;i++)
+        // {
+        //     for(int j=0;j<=1;j++)
+        //     {
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        return dp[0][0];
+    }
